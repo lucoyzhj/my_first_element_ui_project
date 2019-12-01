33a4e51b8dda6981765b60e662e9b4f5a5c4d51f
lucoyzhj <lucoy@163.com>
2019-12-01 17:12:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <el-button @click="startHacking" v-bind:name="start1">Start1</el-button>
+      <el-button @click="startHacking" v-bind:name="start3">Start2</el-button>
+      <el-button @click="startHacking" v-bind:name="start3">Start3</el-button>
+    </div>
+  </div>
+</template>
+/*按钮上的元素怎么才能传递到弹出层？*/
+
+
+<script>
+export default {
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!'+this.name,
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
